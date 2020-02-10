f0de43383742c4b1c6236c1c31c5e89c3c8ed519
= <luiseduardo.0708@gmail.com>
2020-02-10 08:09:34 -0300

    comitando o curso de kafka da alura

---
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,20 @@
+package br.com.alura.ecommerce;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (int i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + "123132,65455,1234";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
+        }
+    }
+}
